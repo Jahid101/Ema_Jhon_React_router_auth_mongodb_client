7bd2a287253114a6c7c73e3d8df68d6c202196f8
Jahid101 <jahidhasananik.official@gmail.com>
2021-04-09 20:05:13 +0600

    normal payment method added

--- src/components/ProcessPayment/ProcessPayment.js
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import SimpleCardForm from './SimpleCardForm';
-import SplitCardForm from './SplitCardForm';
 
 const stripePromise = loadStripe('pk_test_51IeGnnGranyyLnAmLW22rf5qeFwcbqQi1Ky0k9GRwjvcTSEHuHfzew1LPWdXrzeDVTasOEOj9WUF1pzsYZZr5X2r00HZbjsjAB');
 
